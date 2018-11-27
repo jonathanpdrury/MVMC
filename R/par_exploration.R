41a6c394a0f5bc6c4ee7bb57233869eab117e2b0
Jonathan Drury <jonathan.p.drury@gmail.com>
2018-11-27 15:31:05 +0000

    adding some better annotations

--- R/par_exploration.R
+++ R/par_exploration.R
@@ -20,7 +20,9 @@ sim_t_comp_plot(tree_020,c(1,-0.347),0,model="MC",plot=TRUE)
 
 #for DDexp
 log(0.01/sig2_0)/length(tree_020$tip.label)
+#log(rate at tips/rate at root)/treesize
 
+#if, for all negative slope parameter values we fix the rate at the tips to be 0.01
 #sig2_0=0.1
 sim_t_comp_plot(tree_020,c(1,-0.1151293),0,model="DDexp",plot=TRUE)
 
@@ -30,20 +32,22 @@ sim_t_comp_plot(tree_020,c(1,-0.1956012),0,model="DDexp",plot=TRUE)
 #sig2_0=1.0
 sim_t_comp_plot(tree_020,c(1,-0.2302585),0,model="DDexp",plot=TRUE)
 
-
-#sig2_0=0.1, positive slope (same thing as switching starting and endrate)
+#if, for all positive slope parameter values we fix the rate at the root to be 0.01
+#sig2_tip=0.1, positive slope (same thing as switching starting and endrate)
 sim_t_comp_plot(tree_020,c(1,0.1151293),0,model="DDexp",plot=TRUE)
 
-#sig2_0=0.5, positive slope (same thing as switching starting and endrate)
+#sig2_tip=0.5, positive slope (same thing as switching starting and endrate)
 sim_t_comp_plot(tree_020,c(1,0.1956012),0,model="DDexp",plot=TRUE)
 
-#sig2_0=1.0, positive slope (same thing as switching starting and endrate)
+#sig2_tip=1.0, positive slope (same thing as switching starting and endrate)
 sim_t_comp_plot(tree_020,c(1,0.2302585),0,model="DDexp",plot=TRUE)
 
 
 #for DDlin
 (0.01-sig2_0)/length(tree_020$tip.label)
+(rate at tips - rate at root)/treesize
 
+#if, for all negative slope parameter values we fix the rate at the tips to be 0.01
 #sig2_0=0.1
 sim_t_comp_plot(tree_020,c(1,-0.0045),0,model="DDlin",plot=TRUE)
 
@@ -53,11 +57,12 @@ sim_t_comp_plot(tree_020,c(1,-0.0245),0,model="DDlin",plot=TRUE)
 #sig2_0=1
 sim_t_comp_plot(tree_020,c(1,-0.0495),0,model="DDlin",plot=TRUE)
 
-#sig2_0=0.1, positive slope (same thing as switching starting and endrate)
+#if, for all positive slope parameter values we fix the rate at the root to be 0.01
+#sig2_tip=0.1, positive slope (same thing as switching starting and endrate)
 sim_t_comp_plot(tree_020,c(1,0.0045),0,model="DDlin",plot=TRUE)
 
-#sig2_0=0.5, positive slope (same thing as switching starting and endrate)
+#sig2_tip=0.5, positive slope (same thing as switching starting and endrate)
 sim_t_comp_plot(tree_020,c(1,0.0245),0,model="DDlin",plot=TRUE)
 
-#sig2_0=1.0, positive slope (same thing as switching starting and endrate)
+#sig2_tip=1.0, positive slope (same thing as switching starting and endrate)
 sim_t_comp_plot(tree_020,c(1,0.0495),0,model="DDlin",plot=TRUE)
