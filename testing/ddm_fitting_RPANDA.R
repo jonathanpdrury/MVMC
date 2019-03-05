0660f8841c70d7dfcc825ccee211075c7b8c23eb
Jonathan Drury <jonathan.p.drury@gmail.com>
2019-03-05 11:04:31 +0000

    small updates

--- testing/ddm_fitting_RPANDA.R
+++ testing/ddm_fitting_RPANDA.R
@@ -12,8 +12,13 @@ load('~/MVMC/DDexp_neg_data/DDexp_neg_sim_tree_50_root_sig2_1_tip_sig2_3.RData')
 load('~/MVMC/DDexp_neg_data/DDexp_neg_sim_tree_50_root_sig2_1_tip_sig2_2.RData')
 load('~/MVMC/DDexp_neg_data/DDexp_neg_sim_tree_50_root_sig2_1_tip_sig2_1.RData')
 
+source('~/MVMC/R/PhenotypicModel_MV_BETA.R', chdir = TRUE)
+source('~/MVMC/R/PhenotypicADiag_MV_BETA.R', chdir = TRUE)
+
 source('~/MVMC/R/createModel_BM_MV.R', chdir = TRUE)
 source('~/MVMC/R/createModel_DDexp_MV.R', chdir = TRUE)
+
+require(phytools)
 
 res.mat<-matrix(nrow=5*3*3, ncol=20)
 colnames(res.mat)<-c("tree","tree.size","sig2.1","sig2.2","sig2.cov","root.1", "root.2", "r.term.1", "r.term.2", "r.term.cov", "est.sig2.1", "est.sig2.2","est.sig2.cov","est.root.1","est.root.2","est.r.term.1","est.r.term.2","est.r.term.cov","lnL","convergence")
@@ -22,7 +27,7 @@ for(i in 1:5){ #for 5 trees
 
 	for(j in 1:3){ #for 3 sig2 matrices
 	
-		for(k in 1:3){ for 3 slope matrices
+		for(k in 1:3){ #for 3 slope matrices
 		
 		tree<-tree.list[[2]][[i]]
 		
@@ -34,7 +39,7 @@ for(i in 1:5){ #for 5 trees
   			traits[l,2] = data[[l]][2]
 		}
 		colnames(traits) = c("trait.1","trait.2")
-		rownames(traits) = names(sim.results)
+		rownames(traits) = names(data)
 
 		bmm<-createModel_BM_MV(tree)
 		vcv_2<-getTipDistribution(bmm,params=c(0,0,0,0,0))$Sigma
@@ -52,7 +57,7 @@ for(i in 1:5){ #for 5 trees
 		ddm.m0_1<-(ddm.fit$inferredParams[1])
 		ddm.m0_2<-(ddm.fit$inferredParams[2])
 		
-		int<-c(i,50,sig2.matrices[[j]][1,1],sig2.matrices[[j]][2,2],sig2.matrices[[j]][2,1],0,0,pars.list[[2]][[j]][[k]][1,1],pars.list[[2]][[j]][[k]][2,2],pars.list[[2]][[j]][[k]][1,2]),ddm.sig2_1,ddm.sig2_2,ddm.sig2_cov,ddm.m0_1,ddm.m0_2,ddm.r_1,ddm.r_2,ddm.r_cov,-ddm.fit$value, ddm.fit$convergence)
+		int<-c(i,50,sig2.matrices[[j]][1,1],sig2.matrices[[j]][2,2],sig2.matrices[[j]][2,1],0,0,pars.list[[2]][[j]][[k]][1,1],pars.list[[2]][[j]][[k]][2,2],pars.list[[2]][[j]][[k]][1,2],ddm.sig2_1,ddm.sig2_2,ddm.sig2_cov,ddm.m0_1,ddm.m0_2,ddm.r_1,ddm.r_2,ddm.r_cov,-ddm.fit$value, ddm.fit$convergence)
 		
 		res.mat[counter,]<-int
 		print(int)
